6fa66de02ddea67d5d9bef330dba7bcdac870916
Tejal <pateltejal3010@gmail.com>
2025-10-10 00:20:55 +0530

    Commit 09/10/2025

---
+++ PyLearningDir/Class02_091025/T02_Print_Fun.py
@@ -0,0 +1,12 @@
+# comment
+# This code should not be executed
+
+# print function
+print("Hello Again")
+print("Hello All!",91025,"Test Argument","Tejal",True,3.14)
+# Separator
+print("Hello All!",91025,"Test Argument","Tejal",False,sep="_")
+# New Line
+print("Hello All!",91025,"Test Argument","Tejal",end="\n\n")
+print("Hello All!",91025,"Test Argument","Tejal",end="_$_")
+print("Hello All!",91025)
---
+++ PyLearningDir/Class02_091025/T03_Variable.py
@@ -0,0 +1,43 @@
+# Variables
+# Integer
+
+var1=12
+var2=30
+total=var1+var2
+print(total)
+
+#string
+
+name1="hello"
+name1="Jenny"
+name2="world"
+name2="Shinestar"
+name2="Tejal"
+print(name1,name2,sep=" ",end="\n\n")
+print(name2)
+print(end="\n\n")
+
+# float
+
+f=3.14
+g=1.25
+tot=f+g
+print(tot)
+print(end="\n\n")
+
+#boolean
+
+t = True
+t1 = False
+print(t,t1)
+print(end="\n\n")
+
+# List
+
+list1=["star","shine","tejal"]
+print(list1[0],list1[1],list1[2])
+print(list1[1])
+
+#Invalid Variable Name
+# 123abc="123"
+# print(123abc)
