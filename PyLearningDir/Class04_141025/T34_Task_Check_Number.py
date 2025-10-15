be3469426da3c66e7e2968fd6f4feec598ad31f4
Tejal <pateltejal3010@gmail.com>
2025-10-16 00:14:50 +0530

    Committed 14/10/2025

--- PyLearningDir/Class04_141025/T34_Task_Check_Number.py
+++ PyLearningDir/Class04_141025/T34_Task_Check_Number.py
@@ -3,9 +3,13 @@
 num1= 100
 num2 = float(input("Enter number: "))
 
-if num2 > num1:
-    print("Number is greater than 100!")
-else:
-    print("Number is less or equal to 100!")
+print("number is greater than 100" if num2 > num1 else "Number is less or equal to 100")
+
+# if num2 > num1:
+#     print("Number is greater than 100!")
+# else:
+#     print("Number is less or equal to 100!")
+
+
 
 
