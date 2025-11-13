8e9689d402c997f262c3b4cf2d5c0bb022699653
Tejal <pateltejal3010@gmail.com>
2025-11-14 00:22:19 +0530

    Committed 13/11/2025

---
+++ PyLearningDir/Class14_131125/T81_Task_Class_Attribute_Behavior.py
@@ -0,0 +1,53 @@
+# Create a Person class where we will have five attributes and five behaviors.
+# Make sure that each type of function is used, and I want you to also create the print function
+# which will print all the instance variable values.
+
+class School:
+    #Attribute
+    studyclass = None
+    subject= None
+    student= None
+    meduim= None
+    schoolname= None
+
+    #behavior
+
+    # Method without parameters & with return type
+    def m_studyclass(self):
+        return "Study Class: 10th"
+
+    #  Method with one parameter & with return type
+    def m_subject(self, subject):
+        return "Subject:",subject
+
+    # Method with multiple parameters & with return type
+    def m_student(self, name, rollno):
+        print("Student Name: ",name,"Roll No:",rollno)
+
+    #  Method without parameters but returning a value
+    def m_medium(self):
+        print("Medium: English")
+
+    #  Method with parameters & returning combined info
+    def m_schoolname(self, sname, address):
+        return "School:",sname, "Address:",address
+
+LMV=School()
+print(LMV.m_studyclass())                         # No parameter
+print(LMV.m_subject("Science"))                # One parameter
+LMV.m_student("Siya", 21)                # Multiple parameters
+LMV.m_medium()                          # No parameter
+print(LMV.m_schoolname("Bright School", "Vadodara"))  # With parameters
+
+
+
+
+
+
+
+
+
+
+
+
+
